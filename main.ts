b006cf3a54972a16de59ea365a62d1d7a6978b8b
Simone Bonino <smnbonino@gmail.com>
2024-03-22 22:21:44 +0100

    Fix supabase cron

--- main.ts
+++ main.ts
@@ -2,5 +2,10 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
+import { supabase } from "$utils/db.ts";
+
+Deno.cron("KeepAwake", "0 3 * * 1", async () => {
+  await supabase.from("awake").select("*");
+});
 
 await start(manifest, config);
--- utils/crons.ts
+++
@@ -1,5 +0,0 @@
-import { supabase } from "$utils/db.ts";
-
-Deno.cron("Hey, wake up!", "0 3 * * 1", async () => {
-  await supabase.from("awake").select("*");
-});
