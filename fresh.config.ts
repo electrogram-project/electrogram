615b4ac5340a76228fc1f61f9483bf2119981c09
Simone Bonino <smnbonino@gmail.com>
2024-08-09 18:36:10 +0200

    Update fresh.config.ts

--- fresh.config.ts
+++ fresh.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
             "message",
             "callback_query",
             "chat_member",
+            "inline_query",
           ],
           drop_pending_updates: true,
         });
